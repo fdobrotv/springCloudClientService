1b0886cb69c31427bfd4b03a3cd94ae6360e5d09
Abzal Kalimbetov <abzalkalimbetov@gmail.com>
2020-12-09 17:36:56 +0600

    swagger

--- src/main/java/com/fdobrotv/client_service/ClientServiceApplication.java
+++ src/main/java/com/fdobrotv/client_service/ClientServiceApplication.java
@@ -3,6 +3,13 @@ package com.fdobrotv.client_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.PathProvider;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.paths.DefaultPathProvider;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -12,4 +19,13 @@ public class ClientServiceApplication {
         SpringApplication.run(ClientServiceApplication.class, args);
     }
 
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .pathMapping("/clientService")
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(("com.fdobrotv")))
+                .paths(PathSelectors.any())
+                .build();
+    }
 }
