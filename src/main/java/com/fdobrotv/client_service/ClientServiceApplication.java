c1275f98ece75dbabe495947fa00ffe4d8a5fab8
Abzal Kalimbetov <abzalkalimbetov@gmail.com>
2020-12-09 17:59:06 +0600

    swagger config separate file

--- src/main/java/com/fdobrotv/client_service/ClientServiceApplication.java
+++ src/main/java/com/fdobrotv/client_service/ClientServiceApplication.java
@@ -2,11 +2,6 @@ package com.fdobrotv.client_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
@@ -15,15 +10,5 @@ public class ClientServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ClientServiceApplication.class, args);
-    }
-
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.OAS_30)
-                .pathMapping("/clientService")
-                .select()
-                .apis(RequestHandlerSelectors.basePackage(("com.fdobrotv")))
-                .paths(PathSelectors.any())
-                .build();
     }
 }
